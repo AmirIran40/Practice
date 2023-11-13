1aae7f3a7dbe1bb3b9f9ee98849e3615fd826f2a
Amir Shariati <146708912+AmirIran40@users.noreply.github.com>
2023-11-13 11:07:31 +0330

    JS is Fun!!!

---
+++ jalse 15 JS/tasks.js
@@ -0,0 +1,80 @@
+// task 1
+var a = 45;
+if (a >= 88) {
+    console.log('excellent');
+} else if (a <= 87 && a >= 40) {
+    console.log('good');
+} else if (a >= 0 && a <= 39) {
+    console.log('fail'); }
+
+
+    // task 2
+var b = 13;
+var c = 12;
+if (b >= 0 && b <= 12) {
+    console.log('tim is :' + b + 'a.m');
+} else if (b >= 13 && b <= 24) {
+    console.log('tim is :' + (b - 12) + 'p.m');
+    console.log('tim is :' + (b - c) + 'p.m'); }
+
+
+    // task 3
+var e = Esfand;
+if (e == faravardin) {
+    console.log('Its rest time go to vecation');
+} else {
+    console.log('its yours work time work harder!'); }
+
+
+// task 4
+var age = 21;
+if (age >= 0 && age < 18) {
+    console.log("you are Under Legal Age")
+} else if (age >= 18 && age >= 70) {
+    console.log("you are an adult person") }
+
+
+
+    // task 5
+var globalrecord = 2.45;
+var userrecord = 1.73;
+if (userrecord > globalrecord) {
+    console.log("sorry you have time to Try More!")
+} else {
+    console.log("WOW Now We Have a New Record!") }
+
+
+
+// task 6
+    var weeksday;
+while (weeksday >= 0) {
+    console.log(time);
+    weeksday--; }
+
+
+
+    // task 7
+var x = 11;
+x = 0;
+while (x < 15) {
+    if (x == 10) {
+        continue;
+    }
+    console.log(x);
+    x++; }
+
+
+    // task 8
+var learn = 5;
+for (learn = 0; learn <= 3; learn++) {
+    console.log(learn)
+}
+
+
+// task 9
+var num1 = 12;
+var num2 = 9;
+var num3 = 15;
+var nu = num1 + num2 + num3;
+var result = nu / 3
+console.log(result);
